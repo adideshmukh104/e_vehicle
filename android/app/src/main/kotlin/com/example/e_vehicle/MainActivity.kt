f0ec5fd1f886fa83d99c2c776b0cc49fbaad0225
adideshmukh104 <85940635+adideshmukh104@users.noreply.github.com>
2022-04-15 20:16:07 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/example/e_vehicle/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.e_vehicle;
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistran
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
